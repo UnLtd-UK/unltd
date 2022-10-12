010f93ec04b9bfabc40e8a3e0914f93e8fd41d9d
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2022-10-12 14:16:09 +0100

    Progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()]
+  integrations: [svelte(), tailwind(), mdx(), alpinejs()]
 });
