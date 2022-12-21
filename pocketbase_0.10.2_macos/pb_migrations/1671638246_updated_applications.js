9fd1ae557d82c3657d3254bf47df7b7512f8344c
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2022-12-21 16:17:38 +0000

    Cleaned up grant card

---
+++ pocketbase_0.10.2_macos/pb_migrations/1671638246_updated_applications.js
@@ -0,0 +1,25 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("vzy3j3ok3iak6uy")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "kgmzejkk",
+    "name": "social_venture_costs",
+    "type": "bool",
+    "required": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("vzy3j3ok3iak6uy")
+
+  // remove
+  collection.schema.removeField("kgmzejkk")
+
+  return dao.saveCollection(collection)
+})
