3e6de9d84d883a7f2291d28935d914af393a606e
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2023-04-06 14:43:09 +0100

    New meet the team

---
+++ pocketbase_0.14.2_darwin_arm64/pb_migrations/1677690052_updated_fields.js
@@ -0,0 +1,25 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("7fn3fjl77wmfqph")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fedkldu3",
+    "name": "description1",
+    "type": "editor",
+    "required": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("7fn3fjl77wmfqph")
+
+  // remove
+  collection.schema.removeField("fedkldu3")
+
+  return dao.saveCollection(collection)
+})
