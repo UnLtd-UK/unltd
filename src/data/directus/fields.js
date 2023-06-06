6a2501c96f480bf3c12ed2e37ab02be1b344cf2e
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2023-06-06 15:38:38 +0100

    clean up

--- src/data/directus/fields.js
+++ src/data/directus/fields.js
@@ -1,4 +1,4 @@
-let response = await fetch("https://unltd.directus.app/items/applications/?fields[]=name&fields[]=sections.sections_id.*&fields[]=sections.sections_id.fields.fields_id.*&sort[]=sort&fields[]=slug", {
+let response = await fetch("https://unltd.directus.app/items/applications/?fields[]=*&fields[]=sections.sections_id.*&fields[]=sections.sections_id.fields.fields_id.*&sort[]=sort&sort[]=sections.sections_id.sort", {
     method: "GET"
 });
 
