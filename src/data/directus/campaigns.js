441aa464a5c3e48f3937006fdb58721cf08dc6dc
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2023-06-13 11:15:54 +0100

    Fundraiser looking better

---
+++ src/data/directus/campaigns.js
@@ -0,0 +1,8 @@
+let response = await fetch("https://unltd.directus.app/items/campiagns/?fields=*&fields=funds.funds_id.*&fields=sponsors.organisations_id.*&fields=supporters.organisations_id.*&fields=manager.person.*&fields=manager.role.*", {
+    method: "GET"
+});
+
+let json = await response.json();
+let campaigns = json.data;
+
+export { campaigns }
