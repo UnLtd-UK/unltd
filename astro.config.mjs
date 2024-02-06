f14a19c96c482023a8b4de67084ee76b145fbacf
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-02-06 18:07:02 +0000

    Added Tailwind UI react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx(), alpinejs()]
+  integrations: [svelte(), tailwind(), mdx(), alpinejs(), react()]
 });
