b17fd8fd3c6447432641c250d37fa35f74cca22e
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-02-14 11:08:48 +0000

    Links working

--- src/data/fields.js
+++ src/data/fields.js
@@ -1,4 +1,4 @@
-let response = await fetch("https://unltd.directus.app/items/applications/?fields[]=*&fields[]=sections.sections_id.*&fields[]=sections.sections_id.fields.fields_id.*&sort[]=sort&sort[]=sections.sections_id.sort", {
+let response = await fetch("https://unltd.directus.app/items/applications/?fields[]=*&fields[]=sections.sections_id.*&fields[]=sections.sections_id.fields.fields_id.*&sort[]=sort&sort[]=sections.sections_id.sort&filter[status][_eq]=published", {
     method: "GET"
 });
 
--- src/data/pages.js
+++ src/data/pages.js
@@ -1,4 +1,4 @@
-let response = await fetch("https://unltd.directus.app/items/pages/?fields=*", {
+let response = await fetch("https://unltd.directus.app/items/pages/?fields=*&filter[status][_eq]=published", {
     method: "GET"
 });
 
