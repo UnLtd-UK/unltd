beb9d61939bbb11ba0284789f4c97c0228c4e9ec
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-03-12 17:26:10 +0000

    Updated posts

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,6 +4,7 @@ const client = createDirectus('https://unltd.directus.app').with(rest());
 
 const posts = await client.request(
     readItems('posts', {
+        sort: ['sort', '-date_time'],
         filter: {
             status: {
                 _eq: 'published'
