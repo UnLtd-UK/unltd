3598816c9f5e54cd584f855a0991030e94ab95d8
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-03-20 14:00:25 +0000

    colours updated

--- src/components/ui/Popover.jsx
+++ src/components/ui/Popover.jsx
@@ -25,7 +25,7 @@ const PopoverComponent = (props) => {
                             className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-violet-100 dark:hover:bg-violet-900"
                         >
                             <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-violet-50 dark:bg-violet-900 group-hover:bg-violet-50 dark:group-hover:bg-violet-950">
-                                <i className={`${item.icon} h-6 w-6 text-violet-600 dark:text-violet-300 group-hover:text-orange-600 dark:group-hover:text-orange-700 flex justify-center items-center`} aria-hidden="true" />
+                                <i className={`${item.icon} h-6 w-6 text-violet-600 dark:text-violet-300 group-hover:text-amber-600 dark:group-hover:text-amber-700 flex justify-center items-center`} aria-hidden="true" />
                             </div>
                             <div className="flex-auto">
                                 <a href={item.href} className="block font-semibold text-violet-900 dark:text-violet-100">
