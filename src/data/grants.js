5ddb973697c705ef3af268aa7d1b910dff985e80
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-04-05 14:00:54 +0100

    Big changes

--- src/data/grants.js
+++ src/data/grants.js
@@ -3,7 +3,7 @@ import { getCollection } from './load.js';
 const collection = "grants";
 
 const filterOptions = {
-    sort: ['sort', '-date_time'],
+    sort: ['sort'],
     filter: {
         status: {
             _eq: 'published'
@@ -13,76 +13,6 @@ const filterOptions = {
 
 const attach = false;
 
-// const grants = await getCollection(collection, filterOptions, attach);
-
-const grants = [
-    {
-        name: "Starting Up",
-        demographic: "Social entrepreneur",
-        description:
-            "You have an idea or have started to make a difference to people's lives, your community, or have ambitions to create change at a national level.",
-        amount: "8,000",
-        features: [
-            {
-                name: "For social venture costs",
-                icon: "fa-regular fa-circle-check",
-            },
-        ],
-        eligibilities: [
-            {
-                name: "Incorporated",
-                value: "Not yet or less 4 years",
-                icon: "fa-solid fa-pen-nib",
-            },
-            {
-                name: "Selling goods and services",
-                value: "Less than 1 year",
-                icon: "fa-solid fa-bag-shopping",
-            },
-        ],
-        programmes: [
-            { name: "Core", colour: "amber" },
-            { name: "Access to Employment", colour: "violet" },
-            { name: "Financial Inclusion", colour: "red" },
-            { name: "Healthy Ageing", colour: "green" },
-            { name: "Movement for Change", colour: "blue" },
-        ],
-    },
-    {
-        name: "Scaling Up",
-        description:
-            "You are already running a social venture that is making a positive difference and you have compelling evidence of your impact.",
-        amount: "18,000",
-        features: [
-            {
-                name: "For social venture costs",
-                icon: "fa-regular fa-circle-check",
-            },
-            {
-                name: "For social entrepreneur living costs",
-                icon: "fa-regular fa-circle-check",
-            },
-        ],
-        eligibilities: [
-            {
-                name: "Incorporated",
-                value: "Less 4 years",
-                icon: "fa-solid fa-pen-nib",
-            },
-            {
-                name: "Selling goods and services",
-                value: "1 to 4 years",
-                icon: "fa-solid fa-bag-shopping",
-            },
-        ],
-        programmes: [
-            { name: "Core", colour: "amber" },
-            { name: "Access to Employment", colour: "violet" },
-            { name: "Financial Inclusion", colour: "red" },
-            { name: "Healthy Ageing", colour: "green" },
-            { name: "Movement for Change", colour: "blue" },
-        ],
-    },
-];
+const grants = await getCollection(collection, filterOptions, attach);
 
 export { grants }
