706ac46db057f98a130265ed6f1f7c6194ac7eb3
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-04-18 16:11:12 +0100

    Big code change

--- src/components/ui/Dialog.jsx
+++ src/components/ui/Dialog.jsx
@@ -2,14 +2,14 @@ import { Dialog } from '@headlessui/react'
 import DisclosureComp from './Disclosure.jsx';
 import Logo from "../Logo.jsx";
 
-export default function DialogComp({ nav, mobileMenuOpen, setMobileMenuOpen }) {
+export default function DialogComp({ nav, fill, text, text2, bg, bg2, mobileMenuOpen, setMobileMenuOpen }) {
     return (
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <div className="fixed inset-0 z-10" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-violet-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-violet-900/10">
+            <Dialog.Panel className={`fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-violet-900/10 ${bg}`}>
                 <div className="flex items-center justify-between">
                     <a href="/" className="-m-1.5 p-1.5">
-                        <Logo colour="fill-amber-600" />
+                        <Logo colour={fill} />
                     </a>
                     <button
                         type="button"
