5f77e4d51f474acdfcfeccbdede19bdc38680e57
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-04-22 16:07:19 +0100

    Programmes design

--- src/data/fonts.js
+++
@@ -1,8 +0,0 @@
-const fonts = {
-    "Funding Futures Programme": "font-ff",
-    "Healthy Ageing": "font-ha",
-    "Movement for Change": "font-mfc",
-    "Access to employment": "font-ate",
-}
-
-export { fonts }
