e0099cc42fa900276cb6bdf70dc7d39a13465e41
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-05-29 11:28:42 +0100

    Wrangler

--- src/data/posts.js
+++ src/data/posts.js
@@ -3,7 +3,7 @@ import { getCollection } from './load.js';
 const collection = "posts";
 const name = "posts";
 
-const status = context.env.BLOG === 'main' ? 'published' : 'draft';
+const status = context.env.BLOG === 'published' ? 'published' : 'draft';
 
 const filterOptions = {
     sort: ['sort', '-date_time'],
