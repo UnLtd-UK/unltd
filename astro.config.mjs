7e6fb1e1812641ad266a88b193ef52c081643e24
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-07-04 15:08:00 +0100

    Trailing slash fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx(), alpinejs(), react()]
+  integrations: [svelte(), tailwind(), mdx(), alpinejs(), react()],
+  trailingSlash: 'never',
 });
