624f098b4c4dd9b89d5f6446b1fbb6185416d799
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-11-04 12:16:47 +0000

    learn

--- src/components/ui/Panel.jsx
+++ src/components/ui/Panel.jsx
@@ -3,7 +3,7 @@ import { Popover } from '@headlessui/react'
 
 export default function PanelComp({ navs, styles }) {
     return (<Popover.Group className="hidden lg:flex lg:gap-x-12">
-        <a href="https://unltd.notion.site/30d6634e3a804604880ea30e5fd558a1?v=32fbb6ab6e074f50b0855c8e0de0ca44&pvs=74" target="_blank" className={`text-sm font-semibold leading-6 ${styles.text}`}>
+        <a href="/learn" target="_blank" className={`text-sm font-semibold leading-6 ${styles.text}`}>
             Learn
         </a>
         {
