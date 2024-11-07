9f875df53df5c4b8a20387dc6b117b6a020afe2f
Tom Sheppard <18311489+tjsheppard@users.noreply.github.com>
2024-11-07 17:28:03 +0000

    Improved panels

---
+++ src/data/teams.js
@@ -0,0 +1,22 @@
+import { getCollection } from './load.js';
+
+const collection = "our-team";
+const name = "our-team";
+
+const filterOptions = {
+    sort: ['sort', 'name'],
+    filter: {
+        status: {
+            _eq: 'published'
+        },
+        person: {
+            _nnull: true
+        }
+    },
+}
+
+const attach = false;
+
+const ourteam = await getCollection(collection, name, filterOptions, attach);
+
+export { ourteam }
