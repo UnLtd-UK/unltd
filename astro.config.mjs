20fed3fda44466aa920bf41af997b73ee358a22d
Tom Sheppard <sheppardnexus@gmail.com>
2025-02-10 10:34:46 +0000

    Sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), react(), markdoc()],
+  integrations: [alpinejs(), react(), markdoc(), sitemap()],
+  site: 'https://unltd.org.uk',
   trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()],
