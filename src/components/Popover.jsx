881dce8407822d116ec000f11b9cbca75e453df5
Tom Sheppard <sheppardnexus@gmail.com>
2025-03-05 16:49:11 +0000

    Remove FeedbackWidget component and update Popover to handle feedback submission; streamline feedback process and improve form handling

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -12,16 +12,10 @@ export default function FeedbackPopover() {
         setIsError(false);
 
         try {
+            const formData = new FormData(event.target);
             const response = await fetch('/forms', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                type: event.target.type.value,
-                email: event.target.email.value,
-                message: event.target.message.value,
-            }),
+                method: 'POST',
+                body: formData,
             });
 
             if (response.ok) {
@@ -46,11 +40,11 @@ export default function FeedbackPopover() {
                     <p className="flex gap-1"><i className="fa-solid fa-circle-check text-green-500"></i><span className="text-xs text-violet-100">Feedback sent. You have received a confirmation email.</span></p>
                 ) : (
                     <form onSubmit={handleSubmit} className="">
-                        <div class="mb-4">
+                        <div className="mb-4">
                             <fieldset>
                                 <legend className="text-violet-100 text-xs mb-2">What's on your mind?</legend>
 
-                                <div class="flex justify-stretch gap-1">
+                                <div className="flex justify-stretch gap-1">
                                     <input type="radio" id="issue" name="type" disabled={isSubmitting} value="issue" className="hidden peer/option1" />
                                     <label for="issue" className="flex gap-1 items-center cursor-pointer bg-white px-4 py-2 text-xs font-sans border-2 border-gray-700 rounded-full 
             peer-checked/option1:bg-yellow-200 peer-checked/option1:border-yellow-600 
@@ -61,28 +55,28 @@ export default function FeedbackPopover() {
                                     <label for="feature" className="flex gap-1 items-center cursor-pointer bg-white px-4 py-2 text-xs font-sans border-2 border-gray-700 rounded-full 
             peer-checked/option2:bg-blue-200 peer-checked/option2:border-blue-600 
             peer-focus/option2:border-dashed peer-focus/option2:border-blue-700 
-            hover:bg-blue-100 transition"><i class="fa-solid fa-lightbulb text-blue-800"></i><span className="text-blue-900">Feature</span></label>
+            hover:bg-blue-100 transition"><i className="fa-solid fa-lightbulb text-blue-800"></i><span className="text-blue-900">Feature</span></label>
 
                                     <input type="radio" id="other" name="type" disabled={isSubmitting} value="other" className="hidden peer/option3" />
                                     <label for="other" className="flex gap-1 items-center cursor-pointer bg-white px-4 py-2 text-xs font-sans border-2 border-gray-700 rounded-full 
             peer-checked/option3:bg-green-200 peer-checked/option3:border-green-600 
             peer-focus/option3:border-dashed peer-focus/option3:border-green-700 
-            hover:bg-green-100 transition"><i class="fa-solid fa-ellipsis text-green-800"></i><span className="text-green-900">Other</span></label>
+            hover:bg-green-100 transition"><i className="fa-solid fa-ellipsis text-green-800"></i><span className="text-green-900">Other</span></label>
                                 </div>
                             </fieldset>
                         </div>
-                        <div class="mb-4">
-                            <label class="block text-xs font-medium text-violet-100">Email</label>
-                            <input disabled={isSubmitting} name="email" type="email" class="p-2 mt-1 block w-full rounded-md border-1 border-violet-400 text-violet-200 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm" />
+                        <div className="mb-4">
+                            <label className="block text-xs font-medium text-violet-100">Email</label>
+                            <input disabled={isSubmitting} name="email" type="email" className="p-2 mt-1 block w-full rounded-md border-1 border-violet-400 text-violet-200 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm" />
                         </div>
-                        <div class="mb-4">
-                            <label class="block text-xs font-medium text-violet-100">Message</label>
-                            <textarea disabled={isSubmitting} name="message" rows="4" class="p-2 mt-1 block w-full rounded-md border-1 border-violet-400 text-violet-200 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm"></textarea>
+                        <div className="mb-4">
+                            <label className="block text-xs font-medium text-violet-100">Message</label>
+                            <textarea disabled={isSubmitting} name="message" rows="4" className="p-2 mt-1 block w-full rounded-md border-1 border-violet-400 text-violet-200 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm"></textarea>
                         </div>
-                        <div class="cf-turnstile" data-sitekey="0x4AAAAAAA7WX5YoqLQOPiIW"></div>
+                        <div className="cf-turnstile" data-sitekey="0x4AAAAAAA7WX5YoqLQOPiIW"></div>
 
-                        <div class="flex justify-start">
-                            <button disabled={isSubmitting} type="submit" class="cursor-pointer inline-flex items-center px-3 py-1 border border-transparent rounded-md shadow-sm text-xs font-medium text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
+                        <div className="flex justify-start">
+                            <button disabled={isSubmitting} type="submit" className="cursor-pointer inline-flex items-center px-3 py-1 border border-transparent rounded-md shadow-sm text-xs font-medium text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
                                 {isSubmitting ? 'Submitting...' : 'Submit'}
                             </button>
                             
