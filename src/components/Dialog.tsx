75ffaa5149f03c510de31ee716736c67c5ea962a
Tom Sheppard <sheppardnexus@gmail.com>
2025-06-10 13:26:26 +0100

    fix: add backdrop blur effect to Dialog component

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -11,7 +11,7 @@ export default function DialogComponent({ title, description, primaryButton, sec
     <Dialog open={open} onClose={() => { }} className="relative z-10">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in backdrop-blur-xs "
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
